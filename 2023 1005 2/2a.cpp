5a4252f80d83ef45096db3386ea9433f81d92a66
KameguchiYu <yu.kameguchi8010@gmail.com>
2023-10-19 10:13:24 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023 1005 2/2a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "2b.h"
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	cout << "1‚Â–Ú‚Ì”Žš‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢" << endl;
+	cin >> a;
+	cout << "2‚Â–Ú‚Ì”Žš‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢" << endl;
+	cin >> b;
+
+	cout << "‘«‚µ‚½“š‚¦‚Í" << sum(a, b) << "‚Å‚·" << endl;
+}
